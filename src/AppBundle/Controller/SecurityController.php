751a51510de1e1a275d6d5bd0a91b7ec4455a1e6
Melinda-Maria Pop <maria.pop@pitechnologies.ro>
2017-04-05 10:04:02 +0300

    Added new pages, added new blocks

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
---
+++ src/AppBundle/Service/Block/HeaderBlockService.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Sonata\BlockBundle\Block;
+
+use Sonata\BlockBundle\Block\Service\BlockServiceInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Sonata\BlockBundle\Model\BlockInterface;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\CoreBundle\Validator\ErrorElement;
+use Sonata\BlockBundle\Block\Service\AbstractBlockService;
+
+class HeaderBlockService extends AbstractBlockService implements BlockServiceInterface
+{
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultSettings(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'url'      => false,
+            'title'    => 'Insert the rss title',
+            'template' => 'SonataBlockBundle:Block:block_core_rss.html.twig',
+        ));
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     * @param BlockInterface $block
+     */
+    public function buildEditForm(FormMapper $formMapper, BlockInterface $block)
+    {
+        $formMapper
+            ->add('settings', 'sonata_type_immutable_array', array(
+                'keys' => array(
+                    array('url', 'url', array('required' => false)),
+                    array('title', 'text', array('required' => false)),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param ErrorElement $errorElement
+     * @param BlockInterface $block
+     */
+    public function validateBlock(ErrorElement $errorElement, BlockInterface $block)
+    {
+        $errorElement
+            ->with('settings.url')
+            ->assertNotNull(array())
+            ->assertNotBlank()
+            ->end()
+            ->with('settings.title')
+            ->assertNotNull(array())
+            ->assertNotBlank()
+            ->assertMaxLength(array('limit' => 50))
+            ->end()
+        ;
+    }
+
+    /**
+     * @param \Sonata\BlockBundle\Block\BlockContextInterface $blockContext
+     * @param Response|null $response
+     * @return mixed
+     */
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        // merge settings
+        $settings = $blockContext->getSettings();
+        $feeds = false;
+
+        if ($settings['url']) {
+            $options = array(
+                'http' => array(
+                    'user_agent' => 'Sonata/RSS Reader',
+                    'timeout' => 2,
+                )
+            );
+
+            // retrieve contents with a specific stream context to avoid php errors
+            $content = @file_get_contents($settings['url'], false, stream_context_create($options));
+
+            if ($content) {
+                // generate a simple xml element
+                try {
+                    $feeds = new \SimpleXMLElement($content);
+                    $feeds = $feeds->channel->item;
+                } catch (\Exception $e) {
+                    // silently fail error
+                }
+            }
+        }
+
+        return $this->renderResponse($blockContext->getTemplate(), array(
+            'feeds'     => $feeds,
+            'block'     => $blockContext->getBlock(),
+            'settings'  => $settings
+        ), $response);
+    }
+}
